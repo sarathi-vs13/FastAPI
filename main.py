41f18275dc0ae455118881a1d8aa67a617b7927b
sarathi-vs13 <vj.sarathi13@gmail.com>
2024-12-12 19:37:01 +0530

    query parameter for the endpoint books

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 books = [
     {'id':1,'name':'Harrypotter','genre':'Fantasy'},
-    {'id':2,'name':'percy jackson','genre':'Children'},
+    {'id':2,'name':'percy jackson','genre':'Fantasy'},
     {'id':3,'name':'game of thrones','genre':'Dark'},
     {'id':4,'name':'James Bond','genre':'Humour'},
     {'id':5,'name':'shining','genre':'Horror'},
@@ -21,15 +21,17 @@ books = [
 
 
 @app.get('/books')
-async def get_books()  -> list[book]:
+async def get_books(genre:genreURLChoices | None = None)  -> list[book]:
+    if genre:
+        return [
+            book(**b) for b in books if b['genre'].lower() == genre.value.lower()
+        ]
+    
     return [
         book(**b) for b in books
     ]
 
 
-@app.get('/about')
-async def about()  -> str:
-    return "Learning FastAPI"
 
 
 @app.get('/books/{books_id}')
@@ -44,7 +46,7 @@ async def get_books(books_id: int) -> book:
 
 
 @app.get('/genre/{genre}')
-async def get_genre(genre: genreURLChoices) -> list[dict]:
+async def get_genre(genre: genreURLChoices,) -> list[dict]:
     return [
         book for book in books if book['genre'].lower() == genre.value.lower()
     ]
@@ -52,7 +54,11 @@ async def get_genre(genre: genreURLChoices) -> list[dict]:
 
 
 
+@app.get('/about')
+async def about()  -> str:
+    return "Learning FastAPI"
+
 
 @app.get('/test')
-async def about()  -> str:
+async def test()  -> str:
     return "An end point to test changes"
