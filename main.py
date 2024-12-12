bebf0d7984ec3778c62dd836be1b12c3ddd740cf
sarathi-vs13 <vj.sarathi13@gmail.com>
2024-12-12 22:22:58 +0530

    Post method on books and response checks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from schemas import genreURLChoices, book
+from schemas import genreURLChoices, book, bookbase, bookcreate, bookwithID
 
 
 
@@ -9,11 +9,11 @@ app = FastAPI()
 
 
 books = [
-    {'id':1,'name':'Harrypotter','genre':'Fantasy'},
-    {'id':2,'name':'percy jackson','genre':'Fantasy'},
-    {'id':3,'name':'game of thrones','genre':'Dark'},
-    {'id':4,'name':'James Bond','genre':'Humour'},
-    {'id':5,'name':'shining','genre':'Horror'},
+    {'id': 1, 'name': 'Harry Potter', 'genre': 'Fantasy', },
+    {'id': 2, 'name': 'Percy Jackson', 'genre': 'Children', },
+    {'id': 3, 'name': 'Game of Thrones', 'genre': 'Dark', },
+    {'id': 4, 'name': 'James Bond', 'genre': 'Humour', },
+    {'id': 5, 'name': 'Shining', 'genre': 'Horror', },
 ]
 
 
@@ -21,7 +21,7 @@ books = [
 
 
 @app.get('/books')
-async def get_books(genre:genreURLChoices | None = None)  -> list[book]:
+async def get_books(genre:genreURLChoices | None = None)  -> list[bookwithID]:
     if genre:
         return [
             book(**b) for b in books if b['genre'].lower() == genre.value.lower()
@@ -35,9 +35,9 @@ async def get_books(genre:genreURLChoices | None = None)  -> list[book]:
 
 
 @app.get('/books/{books_id}')
-async def get_books(books_id: int) -> book:
+async def get_books(books_id: int) -> bookwithID:
     # Use `next()` to find the book with the matching ID
-    book_instance = next((book(**b) for b in books if b['id'] == books_id), None)
+    book_instance = next((bookwithID(**b) for b in books if b['id'] == books_id), None)
     if book_instance is None:
         # Raise an HTTPException if the book is not found
         raise HTTPException(status_code=404, detail="Book not found")
@@ -62,3 +62,18 @@ async def about()  -> str:
 @app.get('/test')
 async def test()  -> str:
     return "An end point to test changes"
+
+
+
+
+
+
+@app.post("/books")
+async def create_book(book_data: bookcreate) -> bookwithID:
+    id = books[-1]['id']+1
+    new_book = bookwithID(id=id, **book_data.model_dump()).model_dump()
+    books.append(new_book)
+    return new_book
+
+
+    
--- schemas.py
+++ schemas.py
@@ -1,5 +1,6 @@
 from enum import Enum 
 from datetime import date
+from typing import List, Optional
 from pydantic import BaseModel
 
 
@@ -18,10 +19,21 @@ class author(BaseModel):
 
 
 class book(BaseModel):
-    #    {'id':1,'name':'Harrypotter1','genre':'Fantasy'},
     id: int
     name: str
     genre: str
     authors: list[author] = []
 
 
+class bookbase(BaseModel):
+    name: str
+    genre: str
+    authors: Optional[list[author]] = []  # Make authors optional with a default
+
+
+class bookcreate(bookbase):
+    pass
+
+
+class bookwithID(bookbase):
+    id: int
